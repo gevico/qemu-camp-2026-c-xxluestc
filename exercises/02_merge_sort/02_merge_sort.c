1b5dc3d116f0c0f3b1abd54aba6685c1eb02589c
cnb.cM1Qdyf2hKA <4B2vLiRtwoRLKBVQlakp9A+cnb.cM1Qdyf2hKA@noreply.cnb.cool>
2026-04-07 21:02:24 +0800

    02merg sort

--- exercises/02_merge_sort/02_merge_sort.c
+++ exercises/02_merge_sort/02_merge_sort.c
@@ -13,9 +13,49 @@ typedef struct {
 Student students[MAX_STUDENTS];
 Student temp[MAX_STUDENTS];
 
+void merge(int left, int mid, int right) {
+    int i = left;      // 左半部分的起始索引
+    int j = mid + 1;   // 右半部分的起始索引
+    int k = left;      // 临时数组 temp 的索引
+
+    // 比较两个部分的元素，将成绩大的（从高到低）先放入 temp
+    while (i <= mid && j <= right) {
+        if (students[i].score >= students[j].score) {
+            temp[k++] = students[i++];
+        } else {
+            temp[k++] = students[j++];
+        }
+    }
+
+    // 如果左边还有剩余，全部搬过去
+    while (i <= mid) {
+        temp[k++] = students[i++];
+    }
+
+    // 如果右边还有剩余，全部搬过去
+    while (j <= right) {
+        temp[k++] = students[j++];
+    }
+
+    // 把排序好的临时数组拷回原数组 students
+    for (int p = left; p <= right; p++) {
+        students[p] = temp[p];
+    }
+}
+
 void merge_sort(int left, int right) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    // 递归终止条件：当区间只有一个元素时，不需要再分了
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        // 分：递归处理左半边和右半边
+        merge_sort(left, mid);
+        merge_sort(mid + 1, right);
+
+        // 治：合并两个有序序列
+        merge(left, mid, right);
+    }
 }
 
 int main(void) {
